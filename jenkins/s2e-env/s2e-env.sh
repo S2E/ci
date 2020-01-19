6dc7664308a283e77471a3de382a0c2e1508db0a
Vitaly Chipounov <vitaly@cyberhaven.io>
2020-01-19 12:59:52 +0100

    jenkins: updated s2e-env tests
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- jenkins/s2e-env/s2e-env.sh
+++ jenkins/s2e-env/s2e-env.sh
@@ -23,37 +23,6 @@
 
 set -ex
 
-S2E_BUILD_SCRIPTS_REPO="$1"
-
-if [ $# -eq 3 ]; then
-    S2E_ENV_REPO="$2"
-    S2E_ENV_BRANCH="$3"
-
-    git clone "$S2E_BUILD_SCRIPTS_REPO" build-scripts
-    S2E_BUILD_SCRIPTS="$(pwd)/build-scripts"
-
-    git clone "$S2E_ENV_REPO" s2e-env
-    cd s2e-env
-    git checkout "$S2E_ENV_BRANCH"
-elif [ $# -eq 2 ]; then
-    S2E_ENV_DIR="$2"
-    if [ ! -d "$S2E_ENV_DIR" ]; then
-        echo "$S2E_ENV_DIR does not exit"
-        exit 1
-    fi
-
-    git clone "$S2E_BUILD_SCRIPTS_REPO" build-scripts
-    S2E_BUILD_SCRIPTS="$(pwd)/build-scripts"
-    cd $S2E_ENV_DIR
-
-else
-    echo "Usage:"
-    echo "   $0 https://github.com/S2E/build-scripts.git https://github.com/S2E/s2e-env.git s2e_env_branch_name"
-    echo "   $0 https://github.com/S2E/build-scripts.git /path/to/checked_out_folder"
-    exit 1
-fi
-
-
 virtualenv venv
 . venv/bin/activate
 pip install --upgrade pip
@@ -62,4 +31,9 @@ pip install .
 pip install pylint
 
 echo Running pylint...
-pylint -rn --rcfile=$S2E_BUILD_SCRIPTS/pylint_rc s2e_env
+pylint -rn --rcfile=./pylint_rc s2e_env
+
+echo Running tests...
+export TERM=linux
+export TERMINFO=/etc/terminfo
+python setup.py test
